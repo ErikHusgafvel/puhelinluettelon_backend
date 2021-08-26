ee8764ee05bac9608e9bf2ac4a518255d5601239
Erik Husgafvel <husgafvel.erik@gmail.com>
2021-08-26 15:50:00 +0300

    3.6: puhelinluettelon backend step6

--- index.js
+++ index.js
@@ -66,6 +66,26 @@ app.post('/api/persons', (request, response) => {
     
     const body = request.body
 
+    if(!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    } 
+    
+    if(!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const unique = persons.find(person => person.name === body.name)
+
+    if(unique) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const person = {
         id: generateId(),
         name: body.name,
