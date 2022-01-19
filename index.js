f07b49864b0d3387d61e2b925f67cda97b9ef6b6
Erik Husgafvel <husgafvel.erik@gmail.com>
2022-01-19 14:58:12 +0200

    console.log comment out

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
-    console.log(persons)
+    //console.log(persons)
     response.status(204).end()
 })
 
