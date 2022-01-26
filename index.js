0e7ef31bc0d42ce8ddcae8a620cc659ba663933a
Erik Husgafvel <husgafvel.erik@gmail.com>
2022-01-26 19:51:12 +0200

    3.16: puhelinluettelo ja tietokanta, step4

--- index.js
+++ index.js
@@ -65,10 +65,15 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        response.status(200).json(person)
+        if (person) {
+            response.status(200).json(person)
+        } else {
+            response.status(404).end()
+        }        
     })
+    .catch(error => next(error))
 })
 
 
@@ -128,6 +133,18 @@ const unknownEndpoint = (req, res) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
